cf32a0e955021994c7a1e776a4b8590feec9508f
Alan Denison and Joel Delight <alan@>
2016-06-09 13:12:57 -0700

    complete fourth item on bdd, encode not a square letter words

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 var cypher = function(input) {
   var array1 = [];
   var result = "";
-  var stringLength = Math.sqrt(input.length)
-  for(i=0; i< input.length; i += stringLength) {
-  array1.push(input.slice(i, i + stringLength));
+  var userInput = input.replace(/ /gi, "")
+  var stringLength = Math.ceil(Math.sqrt(userInput.length));
+  for(i=0; i< userInput.length; i += stringLength) {
+  array1.push(userInput.slice(i, i + stringLength));
   }
   for(i = 0; i < stringLength; i ++) {
-    for(index = 0; index < stringLength; index ++) {
+    // debugger;
+    for(index = 0; index < array1.length; index ++) {
       result = result + array1[index].charAt(i);
     }
   }
 // debugger;
-  for (index = 0; index < result.length; index += 5) {
+  result = result.replace(/ /gi, "")
+  for (index = 0; index + 5 < result.length; index += 5) {
     i = index / 5;
     result = result.slice(0, index + i + 5) + " " + result.slice(index + i + 5)
   }
