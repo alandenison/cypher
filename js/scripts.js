8f3ab63861dfaed85ea88a5420a28e57b6223f3f
Alan Denison and Joel Delight <joel@>
2016-06-09 11:34:43 -0700

    complete second item on bdd, encode 9 letter words

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 var cypher = function(input) {
   var array1 = [];
   var result = "";
-  array1.push(input.slice(0,2));
-  array1.push(input.slice(2,4));
-  result=array1[0].charAt(0)+array1[1].charAt(0)+array1[0].charAt(1)+array1[1].charAt(1);
+  var stringLength = Math.sqrt(input.length)
+  for(i=0; i< input.length; i += stringLength) {
+  array1.push(input.slice(i, i + stringLength));
+  }
+  for(i = 0; i < stringLength; i ++) {
+    for(index = 0; index < stringLength; index ++) {
+      result = result + array1[index].charAt(i);
+    }
+  }
+  result = result.slice(0,5) + " " + result.slice(5,9);
   return result
 }
